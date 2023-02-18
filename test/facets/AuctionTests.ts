bebf912b63e94d70b0d390e99ae3f717821039a8
ruymaster <mecury1984@outlook.com>
2023-02-17 22:20:40 -0700

    test always with simulated block time

--- test/facets/AuctionTests.ts
+++ test/facets/AuctionTests.ts
@@ -95,6 +95,7 @@ export function suite() {
         AuctionType.TimedAndFixed,
       ];
       await token1.approve(auctionContract.address, toWei(1000));
+      await networkHelpers.time.setNextBlockTimestamp(Math.floor(Date.now() / 1000) + 3600);
       const tx = await auctionContract.createAuction(
         0,
         86400 * 2,
@@ -206,7 +207,7 @@ export function suite() {
         auctionContract.connect(secondBidder).placeBid(1, toWei(9), dai.address, toWei(0.21524)),
       ).to.be.revertedWith('too small bid amount');
       // set the timestamp of the next block but don't mine a new block
-      await networkHelpers.time.setNextBlockTimestamp(Math.floor(Date.now() / 1000) + 86400 * 2);
+      await networkHelpers.time.setNextBlockTimestamp(Math.floor(Date.now() / 1000) + 86400 * 2 + 3601);
       await expect(
         auctionContract.connect(secondBidder).placeBid(1, toWei(20), dai.address, toWei(0.21523)),
       ).to.be.revertedWith('auction is inactive');
