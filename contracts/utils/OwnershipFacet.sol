036b1de5fcae025ce9025fc27f1e7114e73d335e
ruymaster <mecury1984@outlook.com>
2023-06-12 04:53:47 +0300

    upgrade gnus.ai upgradeable contracts package into v4.9.1

--- contracts/utils/OwnershipFacet.sol
+++ contracts/utils/OwnershipFacet.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.17;
 
 import { LibDiamond } from "../libraries/LibDiamond.sol";
-import { IERC173Upgradeable } from "@gnus.ai/contracts-upgradeable-diamond/contracts/interfaces/IERC173Upgradeable.sol";
+import { IERC173Upgradeable } from "../interfaces/IERC173Upgradeable.sol";
 
 contract OwnershipFacet is IERC173Upgradeable {
     function transferOwnership(address _newOwner) external override {
