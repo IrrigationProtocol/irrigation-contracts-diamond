73be4b8fe1daa55538e12d9126a130f57837ed8f
ruymaster <mecury1984@outlook.com>
2023-07-25 18:31:54 +0300

    add test script for auction with bid token id

---
+++ test/utils/interface.ts
@@ -0,0 +1,64 @@
+import { BigNumber } from "ethers";
+
+export interface AuctionData {
+    seller: string;
+    startTime: number;
+    endTime: number;
+    sellToken: string;
+    trancheIndex: BigNumber,
+    sellAmount: BigNumber;
+    minBidAmount: BigNumber;
+    //
+    fixedPrice: BigNumber;
+    priceRangeStart: BigNumber;
+    //
+    priceRangeEnd: BigNumber;
+    reserve: BigNumber;
+    //
+    curBidId: number;
+    incrementBidPrice: BigNumber;
+    //    
+    // sum of bidAmounts of bidders
+    totalBidAmount: BigNumber;
+    bidTokenGroupId: number;
+    maxWinners: number;
+    // bids in [id = curBidId + 1 - availableBidDepth, curBidId] are winners
+    // availableBidDepth <= maxWinners
+    availableBidDepth: number;
+    status: number;
+    auctionType: number;
+}
+
+export interface AuctionSetting {
+    startTime: number;
+    sellToken: string;
+    //
+    trancheIndex: BigNumber,
+    //    
+    minBidAmount: BigNumber;
+    fixedPrice: BigNumber;
+    //
+    priceRangeStart: BigNumber;
+    priceRangeEnd: BigNumber;
+    //
+    sellAmount: BigNumber;
+    reserve: BigNumber;
+    //    
+    incrementBidPrice: BigNumber;
+    endTime: number;
+    bidTokenGroupId: number;
+    maxWinners: number;
+    auctionType: number;
+}
+
+export interface Bid {
+    bidAmount: BigNumber;
+    bidPrice: BigNumber;
+    //
+    bidder: string,
+    //
+    paidAmount: BigNumber;
+    bidTokenId: number;
+    //
+    status: number;
+}
