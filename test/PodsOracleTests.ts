6c1430bbd769c4c63a0b5a0dc95a7cd352afd5fa
ruymaster <mecury1984@outlook.com>
2023-08-24 05:15:02 +0300

    add task for pods price

--- test/PodsOracleTests.ts
+++ test/PodsOracleTests.ts
@@ -4,6 +4,7 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { PodsOracleUpgradeable } from '../typechain-types';
 import { IrrigationDiamond } from '../typechain-types/hardhat-diamond-abi/HardhatDiamondABI.sol';
 import { BigNumber } from 'ethers';
+import { getPriceOfPods } from './utils/price';
 
 export function suite() {
   describe('Irrigation Pods Oracle Testing', async function () {
@@ -18,9 +19,6 @@ export function suite() {
     });
 
     it('Test pods price', async () => {
-      // price calculation based on onchain data
-      // const podPrice = await podsOracle.latestPriceOfPods(toD6(23.68), toD6(9001.46));
-      // console.log('PODS price:', fromWei(podPrice));
       // test price calculation based on simulate data
       const price = await podsOracle.priceOfPods(
         BigNumber.from('81479313214613'),
@@ -32,6 +30,11 @@ export function suite() {
         fromWei(price) >= 8732.838594 && fromWei(price) <= 8732.838595,
         `expected price 8732.838595 but ${fromWei(price)}`,
       );
+      expect(getPriceOfPods(
+        BigNumber.from('81479313214613'),
+        BigNumber.from('9001460000'),
+        BigNumber.from('851463116022336'),
+        BigNumber.from('57799313214613'),)).to.be.eq(BigNumber.from("8732838594832155249593"));
     });
 
     it('pods price for a podline that contains only harvestable pods should be 1 * pods BDV', async () => {
@@ -42,6 +45,7 @@ export function suite() {
         toD6(200_000_000),
       );
       expect(price).to.be.eq(toWei(100));
+      expect(getPriceOfPods(toD6(100_000_000), toD6(100), toD6(300_000_000), toD6(200_000_000))).to.be.eq(toWei(100));
     });
 
     it('Test pods price for a podline that contains harvestable and unharvestable pods', async () => {
@@ -53,6 +57,7 @@ export function suite() {
       );
       // 10M for harvestable part and 9.5M for unharvestable
       expect(price).to.be.eq(toWei(10_000_000 + 9_500_000));
+      expect(getPriceOfPods(toD6(190_000_000), toD6(20_000_000), toD6(300_000_000), toD6(200_000_000))).to.be.eq(toWei(10_000_000 + 9_500_000));
     });
 
   });
