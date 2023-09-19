b19f88147845b09d66705d6d619dd722b69dc0a5
Super Genius <ken+github@gnus.ai>
2023-09-18 17:23:34 -0700

    Need to DEFAULT_ADMIN_ROLE change upon ownership transfer

--- contracts/utils/OwnershipFacet.sol
+++ contracts/utils/OwnershipFacet.sol
@@ -3,11 +3,14 @@ pragma solidity ^0.8.17;
 
 import { LibDiamond } from "../libraries/LibDiamond.sol";
 import { IERC173Upgradeable } from "../interfaces/IERC173Upgradeable.sol";
+import "./IrrigationAccessControl.sol";
 
-contract OwnershipFacet is IERC173Upgradeable {
+contract OwnershipFacet is IERC173Upgradeable, IrrigationAccessControl {
     function transferOwnership(address _newOwner) external override {
         LibDiamond.enforceIsContractOwner();
         LibDiamond.setContractOwner(_newOwner);
+        // this sets the default admin for every ROLE created
+        _grantRole(DEFAULT_ADMIN_ROLE, _newOwner);
     }
 
     function owner() external override view returns (address owner_) {
