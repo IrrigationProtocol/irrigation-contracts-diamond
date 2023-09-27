9db426ac420f0cee4eccf0b6da2b78d579081a20
ruymaster <mecury1984@outlook.com>
2023-09-27 11:25:16 +0300

    update withdrawToken to onlySuperAdmin

--- contracts/core/SprinklerUpgradeable.sol
+++ contracts/core/SprinklerUpgradeable.sol
@@ -141,7 +141,7 @@ contract SprinklerUpgradeable is
     /// @param token token address
     /// @param to destination address
     /// @param amount token amount
-    function withdrawToken(address token, address to, uint256 amount) external onlyAdminRole {
+    function withdrawToken(address token, address to, uint256 amount) external onlySuperAdminRole {
         /// @dev can't withdraw water token
         if (token == address(this)) revert NoWaterWithdraw();
         if (token == Constants.ETHER) {
